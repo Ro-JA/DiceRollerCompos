46b8db9096b9d640c0006ed5c41b86a52d6e1e4f
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-03-08 22:46:12 +0900

    add image

--- app/src/main/java/com/example/dicerollercompos/MainActivity.kt
+++ app/src/main/java/com/example/dicerollercompos/MainActivity.kt
@@ -3,14 +3,17 @@ package com.example.dicerollercompos
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.dicerollercompos.ui.theme.DiceRollerComposTheme
 
@@ -25,15 +28,23 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-}
+
 
 @Composable
 fun DiceWithButtonAndImage(
     modifier: Modifier = Modifier
-)
- {
-
-
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
+        Image(painter = painterResource(R.drawable.dice_1),
+            contentDescription = "1")
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = stringResource(R.string.roll))
+        }
+    }
 }
 
 
